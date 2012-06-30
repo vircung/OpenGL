bef07ba643e288fe3d398a1788caa08bbe199aa9
Jacek Nakonieczny <jacek.nakonieczny@gmail.com>
2012-06-30 12:05:07 +0200

    Kolorowy trójkąt

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -3,12 +3,10 @@
 void display(void){
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glColor3f(1.0, 1.0, 1.0);
-    glBegin(GL_POLYGON);
-        glVertex3f(0.25, 0.25, 0.0);
-        glVertex3f(0.75, 0.25, 0.0);
-        glVertex3f(0.75, 0.75, 0.0);
-        glVertex3f(0.25, 0.75, 0.0);
+    glBegin(GL_TRIANGLES);
+	    glColor3f(1.0, 0.0, 0.0); glVertex3f(0.0, 0.0, 0.0);
+	    glColor3f(0.0, 1.0, 0.0); glVertex3f(1.0, 0.0, 0.0);
+	    glColor3f(0.0, 0.0, 1.0); glVertex3f(0.5, 1.0, 0.0);
     glEnd();
 
 	glFlush();
